91f421f820a2792edb1ab8cb44d8da444a1e7e2e
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-01 09:38:05 +0800

    added FileHandler subclass to auto-create directory for logfile

--- utils/get_logger.py
+++ utils/get_logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 class CustomExceptionFormatter(logging.Formatter):
     def formatException(self, exc_info):
@@ -7,15 +8,21 @@ class CustomExceptionFormatter(logging.Formatter):
         formatted_result = ''.join([' ' * 8 + line + '\n' if result.index(line) != len(result)-1 else ' ' * 8 + line for line in result])
         return formatted_result
 
+class CustomFileHandler(logging.FileHandler):
+    def emit(self, record):
+        dir = os.path.dirname(self.baseFilename)
+        if not os.path.exists(dir):
+            os.makedirs(dir, exist_ok=True, mode=777)
+        super().emit(record)
+
 formatter = CustomExceptionFormatter(fmt="[%(levelname)s] %(asctime)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 def get_logger(name, filename, logging_level=logging.INFO):
     logger = logging.getLogger(name)
     if not logger.handlers:
-        handler = logging.FileHandler(filename)
+        handler = CustomFileHandler(filename, delay=True)
         handler.setFormatter(formatter)
         
         logger.setLevel(logging_level)
         logger.addHandler(handler)
-
     return logger
