b80d26ece5b22586ed154a1982d8c1437a44dc93
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-02 16:10:28 +0800

    added functions to websocket_services.py for starting the bots, sending accounts, user_session, botconfigs to websocket clients. added function in http_services.py to send the question/answer data to specific bot clients

--- services/http_services.py
+++ services/http_services.py
@@ -1,5 +1,12 @@
 from database import schemas
-
+import asyncio
 async def process_question_answer_form(form: schemas.QuestionAnswerForm):
-    print(form.model_dump())
-    return {"message": "Form received"}
+    from services.websocket_services import send_naver_accounts, send
+    await send_naver_accounts()
+    await send(recipient="QuestionBot", message={"question": form.question}, type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="AnswerBot_Advertisement", message={"answer_advertisement": form.answer_advertisement}, type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="AnswerBot_Exposure", message={"answer_exposure": form.answer_exposure}, type="response_data")
+    await asyncio.sleep(1)
+    return {"message": "Form received and has been sent to bots."}
--- services/websocket_services.py
+++ services/websocket_services.py
@@ -1,4 +1,86 @@
 from services.queues import ws_conn_outbound
+from database import models, database, schemas
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+import asyncio
 
 async def process_incoming_message(client_id, message):
-    print(client_id, message)
+    await send(message=message, recipient=client_id)
+
+async def send(message, type: str, recipient: str, exclude: str=""):
+    outbound_msg = {}
+    
+    if not recipient == "all":
+        outbound_msg["recipient"] = recipient
+    else:
+        outbound_msg["recipient"] = "all"
+        outbound_msg["exclude"] = exclude
+    
+    if type == "task":
+        outbound_msg["message"] = {"type": "task", "message": message}
+    elif type == "response_data":
+        outbound_msg["message"] = {"type": "response_data", "data": message}
+    
+    await ws_conn_outbound.put(outbound_msg)
+
+async def get_naver_account(levelup: bool, filters: list = [], db: Session=next(database.get_db_conn())):
+    return schemas.NaverAccount.model_validate(db.query(models.NaverAccount).filter(and_(models.NaverAccount.status == 0, models.NaverAccount.levelup_id == 1)).filter(*filters).first())\
+            if levelup else\
+            schemas.NaverAccount.model_validate(db.query(models.NaverAccount).filter(models.NaverAccount.status == 0).filter(*filters).first())
+
+async def get_account_interactions(username: str, filters: list = [], db: Session=next(database.get_db_conn())):
+    return schemas.AccountInteraction.model_validate(db.query(models.AccountInteraction).filter(models.AccountInteraction.username == username).filter(*filters).first())
+
+async def get_user_session(username: str, db: Session=next(database.get_db_conn())):
+    return schemas.UserSession.model_validate(db.query(models.UserSession).filter(models.UserSession.username == username).first())
+
+async def get_bot_configs(botconfigs_id: int=1, db: Session=next(database.get_db_conn())):
+    return schemas.BotConfigs.model_validate(db.query(models.BotConfigs).filter(models.BotConfigs.id == botconfigs_id).first())
+
+async def send_naver_accounts():
+    from utils import get_string_instances
+
+    await send(recipient="all", message="START", type="task")
+    await asyncio.sleep(5)
+    
+    levelup_account = await get_naver_account(levelup=True)
+    levelup_account_interactions = await get_account_interactions(username=levelup_account.username)
+    
+    to_skip = []
+    while True:
+        questionbot_account = await get_naver_account(levelup=False, filters=[models.NaverAccount.username != levelup_account.username, models.NaverAccount.username.not_in(to_skip)])
+        questionbot_account_interactions = await get_account_interactions(username=questionbot_account.username)
+        if get_string_instances(questionbot_account.username, levelup_account_interactions.interactions) < 1 and get_string_instances(levelup_account_interactions.username, questionbot_account_interactions.interactions) < 1:
+            break
+        to_skip.append(questionbot_account.username)
+        await asyncio.sleep(5)
+    
+    while True:
+        answerbot_account = await get_naver_account(levelup=False, filters=[models.NaverAccount.username != levelup_account.username, models.NaverAccount.username != questionbot_account.username, models.NaverAccount.username.not_in(to_skip)])
+        answerbot_account_interactions = await get_account_interactions(username=answerbot_account.username)
+        if get_string_instances(answerbot_account.username, questionbot_account_interactions.interactions) < 1 and get_string_instances(questionbot_account.username, answerbot_account_interactions.interactions) < 1:
+            break
+        to_skip.append(answerbot_account.username)
+        await asyncio.sleep(5)
+    
+    levelup_account_usersession = await get_user_session(username=levelup_account.username)
+    questionbot_account_usersession = await get_user_session(username=questionbot_account.username)
+    answerbot_account_usersession = await get_user_session(username=answerbot_account.username)
+
+    botconfigs = await get_bot_configs()
+
+    await send(recipient="AnswerBot_Exposure", message=levelup_account.model_dump(), type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="QuestionBot", message=questionbot_account.model_dump(), type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="AnswerBot_Advertisement", message=answerbot_account.model_dump(), type="response_data")
+    await asyncio.sleep(5)
+
+    await send(recipient="AnswerBot_Exposure", message=levelup_account_usersession.model_dump(), type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="QuestionBot", message=questionbot_account_usersession.model_dump(), type="response_data")
+    await asyncio.sleep(1)
+    await send(recipient="AnswerBot_Advertisement", message=answerbot_account_usersession.model_dump(), type="response_data")
+    await asyncio.sleep(5)
+
+    await send(recipient="all", message=botconfigs.model_dump(), type="response_data")
