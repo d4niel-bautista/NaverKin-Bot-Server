37f9448d2d8e83b2318fdcce39770ad3dbb77e83
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-05 08:53:29 +0800

    updated requirements.txt. updated Start Server.bat to check for server variable in .env file

--- Start Server.bat
+++ Start Server.bat
@@ -1,3 +1,36 @@
 call .\venv\Scripts\activate.bat
 
-uvicorn main:server.app --reload
+@echo off
+setlocal enabledelayedexpansion
+
+REM Specify the path to your .env file
+set "envFile=.env"
+
+REM Initialize SERVER
+set "SERVER="
+
+REM Loop through each line in the .env file
+for /f "usebackq tokens=1,* delims==" %%a in ("%envFile%") do (
+  REM Trim leading and trailing spaces from the variable name and value
+  set "varName=%%a"
+  set "varValue=%%b"
+  for %%A in (!varName!) do set "varName=%%~A"
+  for %%B in (!varValue!) do set "varValue=%%~B"
+
+  REM Check if the current line contains "SERVER"
+  if /i "!varName!"=="SERVER" (
+    set "SERVER=!varValue!"
+  )
+)
+
+REM Check if SERVER was found
+if defined SERVER (
+  echo SERVER: !SERVER!
+
+  REM Run uvicorn command with SERVER
+  uvicorn main:server.app --host !SERVER! --reload
+) else (
+  echo SERVER not found in %envFile%
+)
+
+endlocal
