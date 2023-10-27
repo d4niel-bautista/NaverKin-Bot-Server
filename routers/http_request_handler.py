a731a1a2128a8c9a65c5bff79390833a58d256e7
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-27 14:30:23 +0800

    implemented jwt to generate access tokens for authentication. created functions to check if the user is authenticated when sending requests to certain api endpoints otherwise the requests will be rejected

--- routers/http_request_handler.py
+++ routers/http_request_handler.py
@@ -4,43 +4,42 @@ import services.http_services as http_services
 from database import schemas, database
 from sqlalchemy.orm import Session
 from typing import Union
-from services.authentication import authenticate_user, create_token
+from services.authentication import authenticate_user, create_token, get_current_user
 
 class HTTPRequestHandler():
     def __init__(self) -> None:
         self.router = APIRouter()
 
         @self.router.post("/v1/api/question_answer")
-        async def process_question_answer_form(form: Union[schemas.QuestionAnswerForm_1Q1A, schemas.QuestionAnswerForm_1Q2A], db: Session=Depends(database.get_db_conn)):
+        async def process_question_answer_form(form: Union[schemas.QuestionAnswerForm_1Q1A, schemas.QuestionAnswerForm_1Q2A], db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.process_question_answer_form(form, db)
         
         @self.router.post("/v1/api/add_account")
-        async def add_account(account: schemas.NaverAccountCreate, db: Session=Depends(database.get_db_conn)):
+        async def add_account(account: schemas.NaverAccountCreate, db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.add_account(account, db)
         
         @self.router.get("/v1/api/accounts")
-        async def fetch_accounts(db: Session=Depends(database.get_db_conn)):
+        async def fetch_accounts(db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.fetch_accounts(db)
         
         @self.router.patch("/v1/api/update_account")
-        async def update_account(updated_account: schemas.NaverAccount, db: Session=Depends(database.get_db_conn)):
+        async def update_account(updated_account: schemas.NaverAccount, db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.update_account(updated_account, db)
         
         @self.router.get("/v1/api/generate_form_content")
-        async def generate_form_content(db: Session=Depends(database.get_db_conn)):
+        async def generate_form_content(db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.generate_form_content(db)
         
         @self.router.get("/v1/api/prompt_configs")
-        async def get_prompt_configs(db: Session=Depends(database.get_db_conn)):
+        async def get_prompt_configs(db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.fetch_prompt_configs(db)
         
         @self.router.post("/v1/api/prompt_configs")
-        async def update_prompt_configs(prompt_configs_update: schemas.PromptConfigsUpdate, db: Session=Depends(database.get_db_conn)):
+        async def update_prompt_configs(prompt_configs_update: schemas.PromptConfigsUpdate, db: Session=Depends(database.get_db_conn), authenticated: schemas.Admin=Depends(get_current_user)):
             return await http_services.update_prompt_configs(prompt_configs_update, db)
         
         @self.router.post("/v1/api/token")
-        async def generate_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db_conn),
-        ):
+        async def generate_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db_conn)):
             user = await authenticate_user(form_data.username, form_data.password, db)
             if not user:
                 raise HTTPException(status_code=401, detail="Invalid username or password")
--- services/authentication.py
+++ services/authentication.py
@@ -1,9 +1,17 @@
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from services.services import get_admin_account
 from database.models import AdminLogin
 from database.schemas import Admin
+from database.database import get_db_conn
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/api/token")
 
 async def authenticate_user(username: str, password: str, db: Session):
@@ -15,4 +23,25 @@ async def authenticate_user(username: str, password: str, db: Session):
     return Admin.model_validate(user)
 
 async def create_token(user: Admin):
-    return {"token": "test"}
+    token = jwt.encode(user.model_dump(), key=JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return {"access_token": token, "token_type": "bearer"}
+
+async def get_current_user(token: str=Depends(oauth2_scheme), db: Session=Depends(get_db_conn)):
+    try:
+        payload = jwt.decode(token, key=JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        id = payload.get("id")
+        username = payload.get("username")
+        if not id or not username:
+            raise HTTPException(
+                status_code=401, detail="PAYLOAD ERROR: Could not validate user due to missing properties"
+                )
+        try:
+            return await get_admin_account(db=db, filters=[AdminLogin.id == id, AdminLogin.username == username])
+        except:
+            raise HTTPException(
+                status_code=401, detail="This user does not exist"
+                )
+    except JWTError:
+        raise HTTPException(
+            status_code=401, detail="JWT ERROR: Could not decode token"
+            )
